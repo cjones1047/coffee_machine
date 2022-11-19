f9e68080e24ae009a62bd5957fadf6a50096d348
cjones1047 <cjmj1047@gmail.com>
2022-11-19 16:41:37 -0600

    Process correctly asks user to input payment if there are sufficient resources in the machine

--- main.py
+++ main.py
@@ -39,8 +39,20 @@ def run_machine():
         for resource in resources:
             if resource == "money":
                 print(f'{resource}: ${resources[resource]}')
+            elif resource == "coffee":
+                print(f'{resource}: {resources[resource]}g')
             else:
                 print(f'{resource}: {resources[resource]}mL')
+
+    def sufficient_resources(drink):
+        for resource in MENU[drink]["ingredients"]:
+            if resources[resource] < MENU[drink]["ingredients"][resource]:
+                return False
+        return True
+
+    def successful_transaction(drink):
+        print(f"Please insert coins for {drink}:\n")
+        pennies = int(input("Pennies: "))
 
     # TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”. Prompt should show again after
     #  customer drink is dispensed
@@ -49,25 +61,27 @@ def run_machine():
 
     # TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
     # TODO 3. Print report by user entering 'report' which shows the resources left in the coffee machine
-
-    if user_choice == "off":
-        return
-    elif user_choice == "report":
-        run_report()
-
     # TODO 4. Check if resources are sufficient to make the drink the user requests. If resources are insufficient,
     #  should print to user which resources are insufficient
-
     # TODO 5. If resources are sufficient, prompt user to insert coins totalling cost of drink. Calculate the total
     #  value of all coins user inserts
-
     # TODO 6. Check is transaction is successful by making sure that total is equal to at least as much as the cost of
     #  the drink. If what the user inserted is insufficient, then tell them it was so and refund money. If user
     #  inserted money WAS sufficient and too much, print the change being returned and add remainder to money property
     #  in resources. If user inserted money was perfect amount print nothing, just add user's charge to 'money'
     #  property.
+
+    if user_choice == "off":
+        return
+    elif user_choice == "report":
+        run_report()
+    elif user_choice in MENU:
+        if sufficient_resources(drink=user_choice):
+            successful_transaction(drink=user_choice)
+
 #
     # TODO 7. Make coffee and adjust resources accordingly if the transaction was successful. After resources are
     #  deducted, print 'Here is your {user_choice}. Enjoy.' and return to asking the user what they would like.
+
 
 run_machine()
